3573fc96d97200ae262f513b0b67e48c516eeaa3
johnb <jbragato@yahoo.com>
2015-01-14 17:35:29 -0500

    x

--- bottle_mongo_restapi.py
+++ bottle_mongo_restapi.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import json
 import bottle
 from bottle import route, run, request, abort
 import pymongo
-from pymongo import Connection
 
-connection = Connection('localhost', 27017)
-db = connection.mydatabase
+def connect_gridfs_mongodb(hostname=None,db_name=None):
+    import pymongo, gridfs, __builtin__
+    if not hostname:
+        hostname = '127.0.0.1'
+    if not db_name:
+        db_name = '127.0.0.1'
+    mongo = pymongo.MongoClient(hostname, max_pool_size=50, waitQueueMultiple=10)
+    mongo_db = mongo[db_name]
+    #mongo_db = mongo[db_name]
+    mongo_db.authenticate('mongo', 'mongo')
+    fs = ''
+    fs = gridfs.GridFS(mongo_db)
+    return mongo_db, fs
+
+db,fs = connect_gridfs_mongodb(hostname=None,db_name=None)
 
 @route('/documents', method='PUT')
 def put_document():
@@ -19,7 +33,6 @@ def put_document():
         db['documents'].save(entity)
     except ValidationError as ve:
         abort(400, str(ve))
-
 
 @route('/documents/:id', method='GET')
 def get_document(id):
