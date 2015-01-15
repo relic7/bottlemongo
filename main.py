c611517c298392d78b99a0f453365bd801648fbc
johnb <jbragato@yahoo.com>
2015-01-15 13:49:20 -0500

    x

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ import controllers
 DEBUG = True
 debug(DEBUG)
 run(host='localhost', port=8080, reloader=DEBUG)
+
+
+# class MainClass:
+
+# 	def __init__(self):
+# 		self.say_hello()
+
+# 	def say_hello(self):
+# 		print("Hello, World!")
+
+
+# if(__name__ == "__main__"):
+# 	MainClass()
