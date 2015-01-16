0850e3e1671c61c206dc09079443615a2c6f0338
johnb <jbragato@yahoo.com>
2015-01-16 16:06:16 -0500

    x

--- models.py
+++ models.py
@@ -18,21 +18,3 @@ class FileF7(Document):
 #connect(db=DB_NAME,collection='fs.files')
 register_connection('default', name=DB_NAME, username='mongo', password='mongo')
 print dir(register_connection)
-
-
-
-
-from flask import Flask
-from werkzeug.routing import BaseConverter, ValidationError
-from itsdangerous import base64_encode, base64_decode
-from bson.objectid import ObjectId
-from bson.errors import InvalidId
-
-class ObjectIDConverter(BaseConverter):
-    def to_python(self, value):
-        try:
-            return ObjectId(base64_decode(value))
-        except (InvalidId, ValueError, TypeError):
-            raise ValidationError()
-    def to_url(self, value):
-        return base64_encode(value.binary)
